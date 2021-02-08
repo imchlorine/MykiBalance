678e37683521191c959d9e2930c741fa29876761
imchlorine <imchlorine@gmail.com>
2021-02-09 01:43:13 +1100

    Add Myki Pass

--- MykiBalance.js
+++ MykiBalance.js
@@ -1,28 +1,22 @@
-
-// 
-// Myki Balance V1.0
+// Myki Balance V2.0
 // Run on Scriptable
 // Created by Ricky Li on 2020/10/12
-// Last modified on 2020/10/13
+// Last modified on 2021/02/08
 // https://github.com/imchlorine/MykiBalance.git
-// This Script is used for feching Myki Balance from MyKi website api.
-// This script does not suport Myki Pass atm as I do not have a Myki Pass.
-// No idea about the myki pass data will be like. 
-// For the same reason, the passenger type (Full Fare, Concession, Child etc) may not accurate, feel free to let me know how to fix them
-// 
+// This Script is used for feching Myki Balance from MyKi website.
+// The passenger type (Concession, Child etc) may not accurate, cos I only have a full fare Card.
+// Important! This Widget is not affiliated to PTV or Myki. For personal use only.
 
-let cardNumber = "Your Card Number"
-// 
-// Do not Edit below
-// 
+let cardNumber = args.widgetParameter
 
+//let cardNumber = "308425123456789"
 let ptvWebAuth = await getMykitoken()
 
 let card = await getCard()
 
 let widget = await createWidget(card)
 
-if ( ! config.runsInWidget) {
+if (!config.runsInWidget) {
     await widget.presentMedium()
 }
 
@@ -31,109 +25,141 @@ Script.complete()
 
 async function createWidget(card) {
 
- let currentTime = new Date()
- let df = new DateFormatter()
-  df.useMediumDateStyle()
-  df.useShortTimeStyle()
- let dateTxt = df.string(currentTime)
- 
- let mykiBalance = card["mykiBalance"]
- let rangeTxt = ""
- let rangeColor = ""
- let balanceTxt = mykiBalance < 0 ? "- $" + mykiBalance.replace(/-/, "") : "$" + mykiBalance
- 
- switch (true) {
-    case (mykiBalance <= 0):
-        rangeTxt = "No Money"
-        rangeColor = "#ff0000"
-        break;
-    case (mykiBalance < 10):
-        rangeTxt = "Low Balance"
-        rangeColor = "#ffcc00"
-        break;
-    default:
-        rangeTxt = "Happy Travel"
-        rangeColor = "#b6e037"
-        break;
- }
- 
- let passengerCode = card["passengerCode"]
- let passengerTxt = ""
- 
- switch (passengerCode) {
-    case 1:
-        passengerTxt = "Full Fare"
-        break;
-    case 2:
-        passengerTxt = "Concession"
-        break;
-    case 3:
-        passengerTxt = "Child"
-        break;
-    case 4:
-        passengerTxt = "Senior"
-        break;
-     default:
-        passengerTxt = "Full Fare"
-        break;
- }
- 
     let widget = new ListWidget()
+
+    if (card["code"] != 1) {
+        let alertMessage = widget.addText(card["message"])
+        alertMessage.font = Font.boldSystemFont(14)
+        alertMessage.textColor = new Color("#ff0000")
+        return widget
+    }
+
+    card = card["data"][0]
+
+    let currentTime = new Date()
+    let df = new DateFormatter()
+    df.useMediumDateStyle()
+    df.useShortTimeStyle()
+    let dateTxt = df.string(currentTime)
+
+    let mykiBalance = card["mykiBalance"]
+    let rangeTxt = ""
+    let rangeColor = ""
+    let balanceTxt = mykiBalance < 0 ? "- $" + mykiBalance.replace(/-/, "") : "$" + mykiBalance
+    let mykiPass = card["Product"]
+
+    if (config.runsWithSiri) {
+        Speech.speak("You have " + balanceTxt + " left in your Myki money!")
+        if (mykiPass.length > 0) {
+            Speech.speak("You have " + mykiPass[0]["daysRemaining"] + " days remaining in your Myki pass!")
+        }
+    }
+
+    let passengerCode = card["passengerCode"]
+    let passengerTxt = ""
+
+    switch (passengerCode) {
+        case 1: // Verified
+            passengerTxt = "Full Fare"
+            break;
+        case 2: // Unverified
+            passengerTxt = "Concession"
+            break;
+        case 3: // Unverified
+            passengerTxt = "Child"
+            break;
+        case 4: // Unverified
+            passengerTxt = "Senior"
+            break;
+        case 7: // Verified
+            passengerTxt = "Student Concession"
+        default:
+            passengerTxt = "Full Fare"
+            break;
+    }
+
     widget.backgroundColor = new Color("#4a525a")
 
     widget.addSpacer()
+
     let mykiUpdateTime = widget.addStack()
-    let updateTxt =mykiUpdateTime.addText("Last updated ")
-        updateTxt.font = Font.boldSystemFont(12)
-        updateTxt.textColor = new Color("#eeeeee") 
+    let updateTxt = mykiUpdateTime.addText("Last updated ")
+    updateTxt.font = Font.boldSystemFont(10)
+    updateTxt.textColor = new Color("#eeeeee")
 
     let updateTime = mykiUpdateTime.addText(dateTxt)
-        updateTime.font = Font.boldSystemFont(12)
-        updateTime.textColor = new Color("#eeeeee")  
- 
+    updateTime.font = Font.boldSystemFont(10)
+    updateTime.textColor = new Color("#eeeeee")
+
     let mykiTitle = widget.addStack()
-    let mykiLogo = mykiTitle.addText("myki")
-        mykiLogo.font = Font.boldSystemFont(30)
-        mykiLogo.textColor = new Color("#b6e037")  
-        mykiTitle.addSpacer()
+    mykiTitle.centerAlignContent()
 
-    let mykiCode= mykiTitle.addText(rangeTxt)
-        mykiCode.font = Font.boldSystemFont(16)
-        mykiCode.textColor = new Color(rangeColor)
-        mykiCode.url = "googlechrome://www.ptv.vic.gov.au/tickets/myki/#topup"
-        widget.addSpacer()
+    let mykiLogo = mykiTitle.addText("**** " + cardNumber.slice(-4))
+    mykiLogo.font = Font.boldSystemFont(20)
+    mykiLogo.textColor = new Color("#b6e037")
+    mykiTitle.addSpacer()
 
-    let balanceTex = widget.addText(balanceTxt)
-        balanceTex.font = Font.boldSystemFont(40)
-        balanceTex.textColor = new Color("#eeeeee")
-        balanceTex.url = "googlechrome://www.ptv.vic.gov.au/tickets/myki/#topup"
-        widget.addSpacer()
-   
+    let mykiCode = mykiTitle.addText("Top Up >")
+    mykiCode.font = Font.boldSystemFont(20)
+    mykiCode.textColor = new Color("#ffcc00")
+    mykiCode.url = "googlechrome://www.ptv.vic.gov.au/tickets/myki/#topup"
+
+    widget.addSpacer()
+
+
+    if (mykiPass.length > 0) {
+        let middleViewTitle = widget.addStack()
+        let moneyTitle = middleViewTitle.addText("myki money")
+        moneyTitle.font = Font.boldSystemFont(12)
+        moneyTitle.textColor = new Color("#eeeeee")
+        middleViewTitle.addSpacer()
+
+        let passTitle = middleViewTitle.addText("myki pass")
+        passTitle.font = Font.boldSystemFont(12)
+        passTitle.textColor = new Color("#eeeeee")
+    }
+
+    let middleView = widget.addStack()
+    let balanceTitle = middleView.addText(balanceTxt)
+    balanceTitle.font = Font.boldSystemFont(40)
+    balanceTitle.textColor = new Color("#eeeeee")
+    middleView.addSpacer()
+
+    if (mykiPass.length > 0) {
+        let daysRemaining = mykiPass[0]["daysRemaining"].toString()
+        let daysRemainingTitle = middleView.addText(daysRemaining)
+        daysRemainingTitle.font = Font.boldSystemFont(40)
+        daysRemainingTitle.textColor = new Color("#eeeeee")
+    }
+
+    middleView.bottomAlignContent()
+
+    widget.addSpacer()
 
     let bottomView = widget.addStack()
 
     let expireDate = bottomView.addText("expiry date: " + card["mykiCardExpiryDate"])
-        expireDate.font = Font.boldSystemFont(10)
-        expireDate.textColor = new Color("#eeeeee")
-        bottomView.addSpacer()
- 
+    expireDate.font = Font.boldSystemFont(10)
+    expireDate.textColor = new Color("#eeeeee")
+    bottomView.addSpacer()
+
     addSymbol({
-    symbol: 'tram.fill',
-    stack: bottomView,
+        symbol: 'tram.fill',
+        stack: bottomView,
     })
     addSymbol({
-    symbol: 'tram',
-    stack: bottomView,
+        symbol: 'tram',
+        stack: bottomView,
     })
     addSymbol({
-    symbol: 'bus',
-    stack: bottomView,
+        symbol: 'bus',
+        stack: bottomView,
     })
     bottomView.addSpacer()
     let travelType = bottomView.addText(passengerTxt)
-        travelType.font = Font.boldSystemFont(10)
-        travelType.textColor = new Color("#eeeeee")
-        widget.addSpacer()
+    travelType.font = Font.boldSystemFont(10)
+    travelType.textColor = new Color("#eeeeee")
+    widget.addSpacer()
 
     return widget
 }
@@ -145,46 +171,38 @@ function addSymbol({
     color = Color.white(),
     size = 12,
 }) {
-  const _sym = SFSymbol.named(symbol)
-  const wImg = stack.addImage(_sym.image)
-  wImg.tintColor = color
-  wImg.imageSize = new Size(size, size)
+    const _sym = SFSymbol.named(symbol)
+    const wImg = stack.addImage(_sym.image)
+    wImg.tintColor = color
+    wImg.imageSize = new Size(size, size)
 }
 
 async function getMykitoken() {
-  let url = "https://www.ptv.vic.gov.au/tickets/myki"
-  let req = new Request(url)
-  let result = await req.loadString()
-  let matchToken = result.match(/"mykiToken":"([^"]+)","mykiTime":([0-9]+)+/)
-  let mykiToken = matchToken[1].replace(/\\\//g, "/")
-  let mykiTime = matchToken[2]
-  return mykiTime + "-" + mykiToken
+    let url = "https://www.ptv.vic.gov.au/tickets/myki"
+    let req = new Request(url)
+    let result = await req.loadString()
+    let matchToken = result.match(/"mykiToken":"([^"]+)","mykiTime":([0-9]+)+/)
+    let mykiToken = matchToken[1].replace(/\\\//g, "/")
+    let mykiTime = matchToken[2]
+    return mykiTime + "-" + mykiToken
 }
 
 async function getCard() {
-  let url = "https://mykiapi.ptv.vic.gov.au/v2/myki/card"
-  let req = new Request(url)
-  req.method = "POST"
-  let defaultHeaders = {
-  Accept: "application/json",
-  "Content-Type": "application/json"}
-  authHeader = {"x-ptvwebauth": ptvWebAuth}
-  req.headers = {
-    ...defaultHeaders,
-    ...authHeader
-  }
-  let data = {"0": {"mykiCardNumber": cardNumber}}
-  req.body = JSON.stringify(data)
-  let result = await req.loadJSON()
-
-//   console.log(result)
-
-  if (result["code"] == 1) {
-    return result["data"][0]
-  }
-  let alert = new Alert()
-    alert.title = "Error"
-    alert.message = result["message"]
-    await alert.present()
-  return null
+    let url = "https://mykiapi.ptv.vic.gov.au/v2/myki/card"
+    let req = new Request(url)
+    req.method = "POST"
+    let defaultHeaders = {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+    }
+    authHeader = { "x-ptvwebauth": ptvWebAuth }
+    req.headers = {
+        ...defaultHeaders,
+        ...authHeader
+    }
+    let data = { "0": { "mykiCardNumber": cardNumber } }
+    req.body = JSON.stringify(data)
+    let result = await req.loadJSON()
+    log(result)
+    return result
 }
--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# MykiBalance
-Fetch Myki Balance with Scriptable, add to iOS 14 Widget
+# MykiBalance  
+Fetch Myki Balance with Scriptable, add to iOS 14 Widget.    
+
+
+**Important! This Widget is not affiliated to PTV or Myki. For personal use only.**
+
+The passenger type (Full Fare, Concession, Child etc) may not accurate, feel free to let me know how to fix them
+
+## How to add MykiBalance Widget to my iOS14?
+
+1. Download [Scriptable](https://apps.apple.com/us/app/scriptable/id1405459188?ign-mpt=uo%3D4) from App Store
+
+2. Add new Script on Scriptable, copy Everything in [MykiBalance.js](https://github.com/imchlorine/MykiBalance/blob/main/MykiBalance.js) and paste to your new Script
+
+3. At this stage,you may see an error message if you try to run the code. Don't worry, You are almost there.
+
+4. Go to your Widgets library and find Scriptable, make sure pick the Middle one and Add Widget to your home screen (Not suport layout for Small and Large Widget).
+
+5. Long press the Widget and go Edit Widget, Choose the `Script` you Added, set `When Interacting` to `Run Script`, so you can manually update your balance by tapping the widget. Input your Myki Card Number into `Parameter` <br />  
+         
+![setting](https://github.com/imchlorine/MykiBalance/blob/main/setting.jpg)
+
+
+6. Now you may see the Magic, Enjoy! If not, check your card number agian!
+
+  * Tips: Tap "Top up >" on the Widget, you will be redirected to Official Myki Topup Website in Chrome (you need Chrome installed on your phone).
+
+
+## How to add the Script to Siri Shortcuts?
+
+ Go to your Scriptable -> Settings -> Siri Shortcuts -> Add Siri Shortcut -> Select the Myki Balance Script -> Add to Siri  
+ 
+ Now try to ask Siri to check your balance.

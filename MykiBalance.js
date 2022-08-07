d57afdc41cb470cd339b8fa067328c0ae7932294
Ricky <imchlorine@gmail.com>
2022-08-07 23:10:09 +1000

    fixed negative value display issue

--- MykiBalance.js
+++ MykiBalance.js
@@ -45,8 +45,8 @@ async function createWidget(card) {
     let mykiBalance = card["mykiBalance"]
     // force balance to be 2 decimal places
     let mykiBalanceValue = parseFloat(mykiBalance).toFixed(2).toString()
-    let balanceSign = mykiBalance < 0 ? "- $" + mykiBalance.replace(/-/, "") : "$"
-    let balanceTxt = balanceSign + mykiBalance
+    let balanceSign = mykiBalance < 0 ? "- $" : "$"
+    let balanceTxt = balanceSign + mykiBalance.mykiBalance.replace(/-/, "")
     let mykiPass = card["Product"] || []
 
     if (config.runsWithSiri) {
